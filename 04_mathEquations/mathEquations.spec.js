a471e840035279b2716a55d929d3e5e99068edb1
Vincent <Vincentliu8823@gmail.com>
2025-06-13 13:52:18 -0400

    finished js exercises 1-5

--- 04_mathEquations/mathEquations.spec.js
+++ 04_mathEquations/mathEquations.spec.js
@@ -4,19 +4,19 @@ describe('math', () => {
   test('a is 0', () => {
     expect(a).toEqual(0);
   });
-  test.skip('b is 9', () => {
+  test('b is 9', () => { // text.skip skips the text, get rid of the .skip part so test can test this part
     expect(b).toEqual(9);
   });
-  test.skip('c is 66', () => {
+  test('c is 66', () => {
     expect(c).toEqual(66);
   });
-  test.skip('d is 1', () => {
+  test('d is 1', () => {
     expect(d).toEqual(1);
   });
-  test.skip('e is -8', () => {
+  test('e is -8', () => {
     expect(e).toEqual(-8);
   });
-  test.skip('f is 68', () => {
+  test('f is 68', () => {
     expect(f).toEqual(68);
   });
 });
